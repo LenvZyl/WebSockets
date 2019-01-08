8f6a3ab2f683806410d53b6495de111f15a16bcc
Lenx <lenvzyl@gmail.com>
2019-01-08 15:53:07 +0200

    Chat with Sockets

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var socket = require('socket.io')
+
+var app = express()
+
+var server = app.listen(4000, function() {
+    console.log('Listening to requests on port 4000')
+})
+
+app.use(express.static('public'))
+
+//socket setup
+var io = socket(server)
+
+io.on('connection', function(socket) {
+    console.log('socket connected')
+
+    //socket listens on 'chat' sent from UI 
+    socket.on('chat', function(data) {
+        io.sockets.emit('chat', data)
+    })
+})
